89865ce43b39ba206d3e606b21da656a372b7ac6
DoHuuDucSE150712 <ducdhse150712@fpt.edu.vn>
2022-10-18 22:34:06 +0700

    first commit

---
+++ src/java/Model/Account.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Admin
+ */
+public class Account {
+    private int userid;
+    private String fullname;
+    private String userimg;
+    private String username;
+    private String pass;
+    private int phone;
+    private int status;
+    private Role role;
+
+    public Account() {
+    }
+
+    public Account(int userid, String fullname, String userimg, String username, String pass, int phone, int status, Role role) {
+        this.userid = userid;
+        this.fullname = fullname;
+        this.userimg = userimg;
+        this.username = username;
+        this.pass = pass;
+        this.phone = phone;
+        this.status = status;
+        this.role = role;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getUserimg() {
+        return userimg;
+    }
+
+    public void setUserimg(String userimg) {
+        this.userimg = userimg;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    
+    
+}
